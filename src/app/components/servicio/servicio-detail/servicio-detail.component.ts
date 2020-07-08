1c65aaab1c3b7c3c452e4e232159ed50ba9d8cb2
Yesica Mendez <yesiikmm@gmail.com>
2020-07-08 20:33:14 -0300

    Se agrego reporte de un servicio seleccionado.

--- src/app/components/servicio/servicio-detail/servicio-detail.component.ts
+++ src/app/components/servicio/servicio-detail/servicio-detail.component.ts
@@ -5,12 +5,15 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { LoginService } from 'src/app/services/login.service';
 import { ToastrService } from 'ngx-toastr';
 import { Afiliado } from 'src/app/models/afiliado';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 @Component({
   selector: 'app-servicio-detail',
   templateUrl: './servicio-detail.component.html',
   styleUrls: ['./servicio-detail.component.css']
 })
+
 export class ServicioDetailComponent implements OnInit {
 
   servicio: Servicio;
@@ -45,6 +48,7 @@ export class ServicioDetailComponent implements OnInit {
     );
   }
 
+
   /**
    * Selecciono el Afiliado que deseo Eliminar y su indice.
    * @param afiliado Afiliado a eliminar del servicio.
@@ -55,6 +59,7 @@ export class ServicioDetailComponent implements OnInit {
     this.afiliado = afiliado;
     this.indexAfi = index;
   }
+
 
   /**
    * Elimino el Afiliado seleccionado y guardo la modificación
@@ -72,6 +77,118 @@ export class ServicioDetailComponent implements OnInit {
       }
     );
     $('#modalDeleteAfi').modal('hide');
+  }
+
+
+  /**
+   * Generar un Reporte de todos los Servicios mostrados.
+   * Descarga un PDF con los Servicios.
+   */
+  public generarPDF() {
+
+    let sizeEtiqueta = 11;
+
+    var doc = new jsPDF('p', 'mm', 'a4');
+
+    // Agrego un titulo al Reporte.
+    doc.setFontType('bold');
+    doc.setTextColor(0);
+    doc.text('Detalle del Servicio', 20, 25);
+    doc.line(20,26,73,26);
+
+    // Agrego el nombre del Servicio.
+    doc.setFontSize(sizeEtiqueta);
+    doc.setTextColor(100);
+    doc.setFontType('normal');
+    doc.text('Nombre:', 20, 45);
+
+    doc.setFontSize(14);
+    doc.setTextColor(0);
+    doc.setFontType('bold');
+    doc.text('"'+ this.servicio.nombre + '"', 46, 45);
+
+    // Agrego la propiedad activo del Servicio.
+    doc.setFontSize(sizeEtiqueta);
+    doc.setTextColor(100);
+    doc.setFontType('normal');
+    doc.text('Activo:', 20, 65);
+
+    let activo: string;
+    if (this.servicio.activo) {
+      activo = 'Si'
+    } else {
+      activo = 'No'
+    }
+
+    doc.setFontSize(12);
+    doc.setTextColor(0);
+    doc.text(activo, 46, 65);
+
+    // Agrego la descripción de Servicio.
+    doc.setFontSize(sizeEtiqueta);
+    doc.setTextColor(100);
+    doc.text('Descripción:', 20, 85);
+
+    doc.setFontSize(12);
+    doc.setTextColor(0);
+    doc.text(this.servicio.descripcion, 46, 85);
+
+    // Agrego la imagen del Servicio.
+    doc.addImage(this.servicio.imagen, 132, 21, 55, 55);
+
+    // Agrego título para lista de inscriptos.
+    doc.setFontSize(sizeEtiqueta);
+    doc.text('Inscriptos al Servicio', 20, 100);
+    doc.line(20,101,56,101);
+
+    // Genero la tabla de Servicios para el Reporte.
+    doc.autoTable({
+      margin: { top: 106 },
+      styles: {
+        halign: 'left'
+      },
+      headStyles: {
+        halign: 'center'
+      },
+      bodyStyles: {
+        minCellHeight: 25,
+      },
+      columnStyles: {
+        4: { halign: 'center' },
+        6: { halign: 'center' },
+      },
+      html: '#tablaInscriptos',
+      columns: [
+        { header: '#' },
+        { header: 'Apellido', dataKey: 'apellido' },
+        { header: 'Nombres', dataKey: 'nombres' },
+        { header: 'Imagen' },
+        { header: 'DNI', dataKey: 'dni' },
+        { header: 'Email', dataKey: 'email' },
+        { header: 'Teléfono', dataKey: 'telefono' },
+      ],
+
+      // Genera las imagenes de los Servicios.
+      didDrawCell: function (data) {
+        if (data.column.index === 3 && data.cell.section === 'body') {
+          var td = data.cell.raw;
+          var img = td.getElementsByTagName('img')[0];
+          var dim = data.cell.height - data.cell.padding('vertical');
+          doc.addImage(img.src, data.cell.x + 2, data.cell.y + 2, dim, dim);
+        }
+      }
+    });
+
+    // Agrego un pie de página al Reporte.
+    doc.setFontSize(14);
+    doc.setTextColor(0);
+    doc.setFontType('normal');
+    doc.text("Programación y Servicios Web 2020", 65, 280);
+
+    doc.setFontSize(11);
+    doc.text("Desarrollado por Grupo 11", 82, 287);
+
+    doc.save("detalle_servicio");
   }
 
 }
